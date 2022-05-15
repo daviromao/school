f6f5de160f3676cc153e72c4adac37b85e4fe9fc
daviromao <davi.romao0210@gmail.com>
2022-05-15 15:34:49 -0300

    feat: create and read of courses and assessments

---
+++ courses/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import CourseAPIView, AssessmentAPIView
+
+urlpatterns = [
+  path('courses/', CourseAPIView.as_view(), name='courses'),
+  path('assessments/', AssessmentAPIView.as_view(), name='assessments')
+]
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Course, Assessment
+from .serializers import CourseSerializer, AssessmentSerializer
+
+
+class CourseAPIView(APIView):
+  """
+  API of Courses
+  """
+
+  def get(self, request):
+    courses = Course.objects.all()
+    serializer = CourseSerializer(courses, many=True)
+    
+    return Response(serializer.data)
+
+  def post(self, request):
+    serializer = CourseSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class AssessmentAPIView(APIView):
+  """
+  API of Assessments
+  """
+
+  def get(self, request):
+    assessments = Assessment.objects.all()
+    serializer = AssessmentSerializer(assessments, many=True)
+
+    return Response(serializer.data)
+  
+  def post(self, request):
+    serializer = AssessmentSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+  
